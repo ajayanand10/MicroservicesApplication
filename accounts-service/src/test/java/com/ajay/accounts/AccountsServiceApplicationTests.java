9cbb9907d43be3de5f7e5ddb101afedd43926d1f
ajaya <ajaya@apporchid.com>
2024-10-11 16:07:10 +0530

    created accounts service

---
+++ accounts-service/src/test/java/com/ajay/accounts/AccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
